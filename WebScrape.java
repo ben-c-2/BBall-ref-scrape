c3b936e5a4072d7b5635f25cce8e308ac715ecf8
ben-c-2 <56576690+ben-c-2@users.noreply.github.com>
2021-02-27 20:11:01 -0500

    Update WebScrape.java

--- WebScrape.java
+++ WebScrape.java
@@ -12,38 +12,50 @@ import org.jsoup.nodes.Element;
 public class WebScrape {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// URL of game log 
+		
 		final String url =
 				"https://www.basketball-reference.com/players/e/embiijo01/gamelog/2021/";
+		
+		// name of player
+		
 		final String player = 
 				"Joel Embiid";
+		
+		// name of team
+		
 		final String team =
 				"Sixers";
-		int inTot = 0;
-		int outTot = 0;
-		double gp = 0;
-		double gnp = 0;
+		int inTot = 0;		// total score of games played with player
+		int outTot = 0;		// total score of games played without player
+		double gp = 0;		// games played
+		double gnp = 0;		// games missed
 		
 		try {
-			final Document document = Jsoup.connect(url).get();
+			final Document document = Jsoup.connect(url).get();		// connects to the url
 		
-			for(Element row : document.select("tbody tr"))
+			for(Element row : document.select("tbody tr"))			// finds list of game logs
 			{
-				if(row.select(".center:nth-of-type(7)").text().equals(""))
+				if(row.select(".center:nth-of-type(7)").text().equals(""))		// filters out empty rows
 				{
 					continue;
 				}
 				else {
 					
 					final String record =
-							row.select(".center:nth-of-type(7)").text();
+							row.select(".center:nth-of-type(7)").text();		// retrieves win loss data
 					final String play =
-							row.select(".center:nth-of-type(8)").text();
+							row.select(".center:nth-of-type(8)").text();		// retrieves if player played
+					
+					// filters if player played
 					if(row.select(".center:nth-of-type(8)").text().equals("") && !(row.select(".center:nth-of-type(7)").text().equals("Opp")))
 					{
 						inTot += Integer.parseInt(record.replaceAll("[^\\-|^\\d]", ""));
 						gp++;
 					}
+					// filters if player did not play
+					
 					if(row.select(".center:nth-of-type(8)").text().contains("i"))
 					{
 						outTot += Integer.parseInt(record.replaceAll("[^\\-|^\\d]", ""));
@@ -61,5 +73,7 @@ public class WebScrape {
 			ex.printStackTrace();
 		}
 	}
+
+}
 
 }
